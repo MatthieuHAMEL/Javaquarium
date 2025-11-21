2ce766bb24520101ae177fd6115b457be0c6df82
MatthieuHAMEL <hamelmatthieusolo@gmail.com>
2025-11-22 00:51:17 +0100

    Aquarium, Fish, Seaweed

--- ecs_version/src/main.cpp
+++ ecs_version/src/main.cpp
@@ -1,13 +1,46 @@
 #include "flecs.h"
 import std;
-
 using namespace std;
 
+enum class Sex { F, M };
+
+struct Fish {
+  std::string name;
+  Sex s;
+};
+struct Seaweed{};
+
+class Aquarium
+{
+public:
+  Aquarium() : m_world{} {
+  }
+  Aquarium& add_fish(std::string name, Sex sex) {
+    println("Adding {}", name);
+    m_world.entity(name.c_str()) // facilitates the queries
+      .set<Fish>({std::move(name), sex});
+    return *this;
+  }
+  Aquarium& add_seaweed(size_t n = 1) {
+    for (size_t k = 0; k < n; ++k) {
+      println("Adding a seaweed");
+      m_world.entity().add<Seaweed>();
+    }
+    return *this;
+  }
+
+  
+private:
+  flecs::world m_world;
+};
+
 auto main() -> int {
-  flecs::world world;
-
-  auto someEntity = world.entity();
-  println("someEntity is alive ? -> {}", someEntity.is_alive());
-
+  Aquarium aq;
+  aq.add_fish("Bob", Sex::M)
+    .add_fish("Brenda", Sex::F)
+    .add_fish("Rhonda", Sex::F)
+    .add_fish("Francesca", Sex::F)
+    .add_fish("Gerard", Sex::M)
+    .add_seaweed(5);
   return 0;
 }
