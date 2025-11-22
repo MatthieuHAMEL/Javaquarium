39beae9f994503f94973027370f9d1bebf5040b8
MatthieuHAMEL <hamelmatthieusolo@gmail.com>
2025-11-22 18:15:53 +0100

    1.1 (simple aquarium ECS) done

--- ecs_version/src/main.cpp
+++ ecs_version/src/main.cpp
@@ -6,7 +6,7 @@ enum class Sex { F, M };
 
 struct Fish {
   std::string name;
-  Sex s;
+  Sex sex;
 };
 struct Seaweed{};
 
@@ -19,19 +19,31 @@ public:
     println("Adding {}", name);
     m_world.entity(name.c_str()) // facilitates the queries
       .set<Fish>({std::move(name), sex});
+    ++m_fishCount;
     return *this;
   }
   Aquarium& add_seaweed(size_t n = 1) {
     for (size_t k = 0; k < n; ++k) {
       println("Adding a seaweed");
       m_world.entity().add<Seaweed>();
+      ++m_seaweedCount;
     }
     return *this;
   }
 
-  
+  void performTimestep() {
+    println("-----");
+    println("There are {} seaweeds and {} fishes:", m_seaweedCount, m_fishCount);
+
+    m_world.each<Fish>([](Fish const& fish) {
+      std::cout << " - " << fish.name << " ("
+                << (fish.sex == Sex::M ? "M" : "F") << ")\n";
+    });
+  }
 private:
   flecs::world m_world;
+  size_t m_fishCount = 0;
+  size_t m_seaweedCount = 0;
 };
 
 auto main() -> int {
@@ -41,6 +53,10 @@ auto main() -> int {
     .add_fish("Rhonda", Sex::F)
     .add_fish("Francesca", Sex::F)
     .add_fish("Gerard", Sex::M)
-    .add_seaweed(5);
+    .add_seaweed(10);
+
+  for (size_t i = 0; i < 10; ++i) {
+    aq.performTimestep();
+  }
   return 0;
 }
