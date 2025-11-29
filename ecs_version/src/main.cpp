3bb3d520714f9251c046101f617ea7c0bc63566a
MatthieuHAMEL <hamelmatthieusolo@gmail.com>
2025-11-29 15:48:46 +0100

    Preparing for healthpoints handling

--- ecs_version/src/main.cpp
+++ ecs_version/src/main.cpp
@@ -13,6 +13,10 @@ struct Fish {
 };
 struct Seaweed{};
 
+struct Living {
+  int healthPoints;
+};
+
 // The fish species
 struct SpeciesR{}; // Relationship symbol
 struct Bass{}; struct Tuna{}; struct ClownFish{}; struct Grouper{}; struct Sole{}; struct Carp{};
@@ -29,7 +33,6 @@ template<> struct Characteristics<ClownFish> { using feeding = Carnivorous; };
 template<> struct Characteristics<Grouper> { using feeding = Herbivorous; };
 template<> struct Characteristics<Sole> { using feeding = Herbivorous; };
 template<> struct Characteristics<Carp> { using feeding = Herbivorous; }; // TODO replace all of this by is_a (component inheritance)
-
 
 std::mt19937 rng{std::random_device{}()};
 
@@ -56,61 +59,66 @@ public:
     m_seaweedQ {m_world.query_builder<Seaweed>().build()},
     m_fishQ {m_world.query_builder<Fish>().build()}
   {
-    // Herbivorous fishes eat seaweeds
-    m_world.system<Fish>().with<FeedingR, Herbivorous>()
+    m_world.component<Living>().add(flecs::OnInstantiate, flecs::Inherit);
+    m_livingPrefab = m_world.prefab().set<Living>({ .healthPoints = 10 });
+    
+    m_world.system<Fish>()
       // enables defer_suspend so that the victim is really destroyed.
       // Otherwise several fishes eat the same seaweed because of flecs commands queuing.
       .immediate()
-      .each([this](Fish const& f) {
-        // Look for a seaweed to eat
-        m_seaweedQ.run([&](flecs::iter& it) {
-          if (it.next()) {
-            auto victim = it.entity(0); // The first seaweed I find
-            m_world.defer_suspend();
-            victim.destruct(); // yum yum
-            m_world.defer_resume();
-          } else {
-            cout << f.name << " can't find any seaweed!\n";
-          }
-        });
-      });
-
-    // Carnivorous fishes eat other fishes
-    m_world.system<Fish>().with<FeedingR, Carnivorous>().immediate()
-      .each([&](flecs::entity predator, Fish const& f) {
-        if (m_world.count<Fish>() == 1) { // There's only one remaining fish in the aquarium
-          cout << "One fish left! " << f.name << " can't eat!\n";
-          return; // Otherwise the below do..while will never end!
+      .each([this](flecs::entity predator, Fish const& f)
+      {
+        if (predator.has<FeedingR, Herbivorous>()) {
+          // Look for a seaweed to eat
+          m_seaweedQ.run([&](flecs::iter& it) {
+            if (it.next()) {
+              auto victim = it.entity(0); // The first seaweed I find
+              m_world.defer_suspend();
+              victim.destruct(); // yum yum
+              m_world.defer_resume();
+            } else {
+              cout << f.name << " can't find any seaweed!\n";
+            }
+          });
         }
+        else { // Carnivorous fishes eat other fishes
+          assert((predator.has<FeedingR, Carnivorous>()));
+          if (m_world.count<Fish>() == 1) { // There's only one remaining fish in the aquarium
+            cout << "One fish left! " << f.name << " can't eat!\n";
+            return; // Otherwise the below do..while will never end!
+          }
 
-        flecs::entity victim;
-        do {
-          victim = random_fish(m_fishQ);
-        } while(victim == predator); // the predator shouldn't eat itself!
-        cout << f.name << " is eating " << victim.get<Fish>().name << '\n';
-        m_world.defer_suspend();
-        victim.destruct();
-        m_world.defer_resume();
+          flecs::entity victim;
+          do {
+            victim = random_fish(m_fishQ);
+          } while(victim == predator); // the predator shouldn't eat itself!
+          cout << f.name << " is eating " << victim.get<Fish>().name << '\n';
+          m_world.defer_suspend();
+          victim.destruct();
+          m_world.defer_resume();
+        }
       });
   }
+  
   template<typename SPECY>
   Aquarium& add_fish(std::string name, Sex sex) {
     using Feeding = typename Characteristics<SPECY>::feeding;
     
     m_world.entity(name.c_str()).set<Fish>({std::move(name), sex})
+      .is_a(m_livingPrefab)
       .add<SpeciesR, SPECY>() // Tuna, Bass, ClownFish...
       .template add<FeedingR, Feeding>(); // Herbivorous/Carnivorous
     return *this;
   }
   Aquarium& add_seaweed(size_t nb = 1) {
     for (size_t i = 0; i < nb; ++i) {
-      m_world.entity().add<Seaweed>();
+      m_world.entity().add<Seaweed>().is_a(m_livingPrefab);
     }
     return *this;
   }
 
   void tick() {
-    m_world.progress();
+    m_world.progress(); // runs the systems
     printAquarium();
   }
 
@@ -123,6 +131,7 @@ public:
   }
 private:
   flecs::world m_world;
+  flecs::entity m_livingPrefab;
   flecs::query<Seaweed> m_seaweedQ;
   flecs::query<Fish> m_fishQ;
 };
