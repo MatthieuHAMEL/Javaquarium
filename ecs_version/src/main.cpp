424c23267e02dc60393d98d64f7f4627824996c4
MatthieuHAMEL <hamelmatthieusolo@gmail.com>
2025-11-29 17:42:47 +0100

    2.1, 2.2, 3.1 (ageing) -> OK

--- ecs_version/src/main.cpp
+++ ecs_version/src/main.cpp
@@ -15,6 +15,8 @@ struct Seaweed{};
 
 struct Living {
   int healthPoints;
+  unsigned age; // in number of turns
+  static constexpr unsigned MAX_AGE = 20;
 };
 
 // Returns whether the Living is dead or not
@@ -23,7 +25,9 @@ bool changeHealth(Living& iLiving, int delta) {
   return (iLiving.healthPoints <= 0);
 }
 
+// Applies only to a fish
 bool isHungry(Living const& iLiving) {
+  assert(iLiving.healthPoints > 0);
   return iLiving.healthPoints <= 5;
 }
 
@@ -44,6 +48,7 @@ template<> struct Characteristics<Grouper> { using feeding = Herbivorous; };
 template<> struct Characteristics<Sole> { using feeding = Herbivorous; };
 template<> struct Characteristics<Carp> { using feeding = Herbivorous; }; // TODO replace all of this by is_a (component inheritance)
 // (Carp prefab is_a<Herbivorous>) -> adding a carp == adding a Carp with Herbivorous
+// Still, there is the need to construct one prefab for each fish specy.
 
 std::mt19937 rng{std::random_device{}()};
 
@@ -73,9 +78,21 @@ public:
     m_world.component<Living>().add(flecs::OnInstantiate, flecs::Override);
     m_livingPrefab = m_world.prefab().set<Living>({ .healthPoints = 10 });
 
+    // Ageing: all living beings get older at every turn
+    // It doesn't need to be immediate() since this system's logic doesn't rely on previous fishes being actually destroyed.
+    m_world.system<Living>()
+      .each([this](flecs::entity e, Living& living) {
+        if (++living.age >= Living::MAX_AGE) {
+          if (e.has<Fish>()) {
+            cout << e.get<Fish>().name << " has died from old age!\n";
+          }
+          immediateDestroy(e);
+        }
+      });
+
     // All seaweed get 1 HP at each turn
     m_world.system<Seaweed, Living>()
-      .each([](flecs::entity _e, Seaweed const&, Living& living) {
+      .each([](Seaweed const&, Living& living) {
         changeHealth(living, 1);
       });
     
@@ -86,9 +103,8 @@ public:
         // The fish gets hungry at every turn: lose one HP
         if (changeHealth(living, -1)) {
           cout << f.name << " is dead because it was too hungry!\n";
-          m_world.defer_suspend();
-          predator.destruct();
-          m_world.defer_resume();
+          immediateDestroy(predator);
+          return;
         }
 
         if (!isHungry(living)) {
@@ -102,9 +118,7 @@ public:
             if (it.next()) {
               auto victim = it.entity(0); // The first seaweed I find
               if (changeHealth(victim.get_mut<Living>(), -2)) { // Seaweed HP has reached 0
-                m_world.defer_suspend();
-                victim.destruct();
-                m_world.defer_resume();
+                immediateDestroy(victim);
               }
               // The predator gets +3 HPs for eating a seaweed.
               changeHealth(living, 3);
@@ -115,26 +129,25 @@ public:
         }
         else { // Carnivorous fishes eat other fishes
           assert((predator.has<FeedingR, Carnivorous>()));
+          
           if (m_world.count<Fish>() == 1) { // There's only one remaining fish in the aquarium
-            cout << "One fish left! " << f.name << " can't eat!\n";
+            cout << f.name << " is the only fish left! It can't eat!\n";
             return; // Otherwise the below do..while will never end!
           }
 
           flecs::entity victim;
           do {
             victim = random_fish(m_fishQ);
-          } while(victim == predator); // the predator shouldn't eat itself!
+          } while(victim == predator); // the predator shouldn't eat itself (nor try to do so)
 
           if (victim.target<SpeciesR>() == predator.target<SpeciesR>()) {
             cout << f.name << " wanted to eat " << victim.get<Fish>().name << " but they are of the same specy!\n";
-            return;
+            return; // There is no other chance.
           }
           cout << f.name << " is eating " << victim.get<Fish>().name << '\n';
           if (changeHealth(victim.get_mut<Living>(), -4)) { // The victim is dead
             cout << victim.get<Fish>().name << " is dead... RIP\n";
-            m_world.defer_suspend();
-            victim.destruct();
-            m_world.defer_resume();
+            immediateDestroy(victim);
           }
           changeHealth(living, 5); // The predator gets HPs
         }
@@ -142,19 +155,25 @@ public:
   }
   
   template<typename SPECY>
-  Aquarium& add_fish(std::string name, Sex sex) {
+  Aquarium& add_fish(std::string name, Sex sex, unsigned age=0) {
     using Feeding = typename Characteristics<SPECY>::feeding;
     
-    m_world.entity(name.c_str()).set<Fish>({std::move(name), sex})
-      .is_a(m_livingPrefab)
+    auto e = m_world.entity(name.c_str()).set<Fish>({std::move(name), sex})
+      .is_a(m_livingPrefab) // (TODO) what about adding the "<flecs::With, Living>" trait on Fish and Seaweed?
       .add<SpeciesR, SPECY>() // Tuna, Bass, ClownFish...
       .template add<FeedingR, Feeding>(); // Herbivorous/Carnivorous
+    e.template get_mut<Living>().age = age;
     return *this;
   }
   Aquarium& add_seaweed(size_t nb = 1) {
     for (size_t i = 0; i < nb; ++i) {
       m_world.entity().add<Seaweed>().is_a(m_livingPrefab);
     }
+    return *this;
+  }
+  Aquarium& add_seaweed_with_age(unsigned age) {
+    auto e = m_world.entity().add<Seaweed>().is_a(m_livingPrefab);
+    e.get_mut<Living>().age = age;
     return *this;
   }
 
@@ -174,6 +193,13 @@ public:
     });
   }
 private:
+  // Must be called from an immediate system. Otherwise it won't be immediate.
+  void immediateDestroy(flecs::entity iEntity) {
+    m_world.defer_suspend();
+    iEntity.destruct();
+    m_world.defer_resume();
+  }
+
   flecs::world m_world;
   flecs::entity m_livingPrefab;
   flecs::query<Seaweed> m_seaweedQ;
@@ -182,10 +208,10 @@ private:
 
 auto main() -> int {
   Aquarium aq;
-  aq.add_fish<Bass>("Bob", Sex::M)
+  aq.add_fish<Bass>("Bob", Sex::M, /*age*/ 10)
     .add_fish<Tuna>("Brenda", Sex::F)
     .add_fish<Grouper>("Rhonda", Sex::F)
-    .add_fish<ClownFish>("Francesca", Sex::F)
+    .add_fish<ClownFish>("Francesca", Sex::F, /*age*/ 4)
     .add_fish<Tuna>("Gerard", Sex::M)
     .add_fish<Sole>("Samantha", Sex::F)
     .add_seaweed(5);
